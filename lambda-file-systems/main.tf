3d194222bf8d520a614077cb6123a0c1e61e037c
Jagadeesan Murugesan <jmurugesan@sinmackjemd6m.paloaltonetworks.local>
2020-11-30 23:00:08 +0530

    Modified files:[main.tf]

--- lambda-file-systems/main.tf
+++ lambda-file-systems/main.tf
@@ -157,3 +157,5 @@ resource "aws_lambda_function" "example_lambda" {
   depends_on = ["aws_efs_mount_target.mount_target_az1", "aws_efs_mount_target.mount_target_az2"]
 
 }
+
+# Modified [main.tf] on [2020-11-30 23:00:08.736113]
